b0febd1fde98472a17867e0423a5a2d30a4c6f38
ayxan65 <memmedliayxan0@gmail.com>
2023-12-29 21:01:50 +0400

    added

--- js-1/script.js
+++ js-1/script.js
@@ -1,17 +1,15 @@
-function calculateTriangle() {
+
     let a = +prompt("a nı daxil edin");
     let b = +prompt("b nı daxil edin");
     let c = +prompt("c nı daxil edin");
   
     if (a + b <= c || b + c <= a || a + c <= b) {
       console.log("Bele 3 bucaq mumkun deyil.");
-      return; 
+     
     }
-  
+  else{
     let d = a + b + c;
     console.log(d);
- 
   }
-  
-  calculateTriangle(); 
+
   
