89e0455a42c4ece825e889f3286e2618a8dfaaf3
ayxan65 <memmedliayxan0@gmail.com>
2024-01-10 17:21:33 +0400

    task added

---
+++ js-3/app.js
@@ -0,0 +1,13 @@
+let a=2000
+let b=99999
+let c="AZN"
+let sahe=+prompt("saheni daxil edin")
+let qiymet=+prompt("qiymeti daxil edin")
+let val=prompt("valyutanı daxil edin")
+
+if(sahe===a && qiymet===b && val===c){
+   console.log("evi alıram");
+}
+else{
+    console.log("almıram")
+}
