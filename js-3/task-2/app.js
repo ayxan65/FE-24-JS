776ecaa095fa98d8f19df301d439acd7393ca2ab
ayxan65 <memmedliayxan0@gmail.com>
2024-01-10 20:37:36 +0400

    task added

---
+++ js-3/task-2/app.js
@@ -0,0 +1,12 @@
+const age=+prompt("yasınızı daxil edin")
+const gender=prompt("cinsinizi daxil edin")
+if(gender==="kişi" && age>=18){
+    console.log("xoş gelmisiniz cenab ")
+ 
+}
+else if(gender==="qadın"&&age>=18){
+    console.log("xoş gelmisiniz xanım");
+}
+else{
+    console.log("girişiniz qadağandır");
+}
