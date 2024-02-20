72dc9b7a40495bf4619a6d8552927ad99e47a617
ayxan65 <memmedliayxan0@gmail.com>
2024-02-20 22:51:29 +0400

    .

--- js-1/task-3/app.js
+++ js-1/task-3/app.js
@@ -1,6 +1,18 @@
-let a= +prompt("1 ci terefi daxil edin: ")
-let b= +prompt("2 ci terefi daxil edin: ")
-let c= +prompt("3 ci terefi daxil edin: ")
-let p=(a+b+c)/2
-let s=Math.sqrt(p*(p-a)*(p-b)*(p-c))
-console.log("area="+s);
+// let a= +prompt("1 ci terefi daxil edin: ")
+// let b= +prompt("2 ci terefi daxil edin: ")
+// let c= +prompt("3 ci terefi daxil edin: ")
+// let p=(a+b+c)/2
+// let s=Math.sqrt(p*(p-a)*(p-b)*(p-c))
+// console.log("area="+s);
+
+
+
+const object={
+
+    age:21,
+    getYear:()=>{
+        age=+prompt("yasi daxil edin:")
+        console.log(age);
+    }
+}
+object.getYear()
