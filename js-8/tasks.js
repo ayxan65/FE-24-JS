1524d7a4c22c22f48f6dcfe3ac229dce5180c3bf
ayxan65 <memmedliayxan0@gmail.com>
2024-02-27 23:21:47 +0400

    task added

---
+++ js-8/tasks.js
@@ -0,0 +1,59 @@
+// let arr=["Ayxan","Amin","Anar"];
+
+// arr1=arr.push("Eli")
+// console.log(arr);
+
+
+// *******************************************
+
+// TASK-2
+
+// let arr=["Ayxan","Alex","Jessica"];
+// arr[1]="classifaed"
+// console.log(arr);
+
+
+// *************************************************
+
+// TASK-3
+
+// let arr = ['New Hope','The Empire Strikes Back','Return of the JDİ'];
+
+// for (let i = 0; i < arr.length; i++) {
+
+
+//     console.log(`episode ${i + 4}: ${arr[i]}`);
+
+
+// }
+
+
+//************************************************** */
+
+
+// TASK-4
+
+// let a=prompt("5 eded daxil edin: ");
+// let arr=a.split(",");
+// console.log(arr);
+// TheBiggestNumber=arr[0];
+// for(elements in arr){
+//     if(arr[elements]>TheBiggestNumber)
+//     {
+//         TheBiggestNumber=arr[elements]
+//     }
+// }
+// console.log(`en böyük eded: ${TheBiggestNumber}`);
+
+
+/*********************************************************** */
+
+
+//TASK-5
+
+
+// let a=prompt("ededler daxil edin")
+// let arr = a.split(",")
+// let arr1=arr.join(";")
+// console.log(arr1);
+
