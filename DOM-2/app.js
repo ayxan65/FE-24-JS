7274aecd401c08ada0151dde6581ef0cb3d20d0b
ayxan65 <memmedliayxan0@gmail.com>
2024-03-06 00:16:51 +0400

    task added

---
+++ DOM-2/app.js
@@ -0,0 +1,32 @@
+
+
+let red=document.getElementById("red")
+let green=document.getElementById("green")
+let purple=document.getElementById("purple")
+let blue=document.getElementById("blue")
+
+
+function redclick(){
+        
+document.body.style.background="red"
+   
+
+}
+function greenclick(){
+    document.body.style.background="green"
+}
+function purpleclick(){
+    document.body.style.background="purple"
+
+}
+function blueclick(){
+    document.body.style.background="blue"
+
+}
+blue.onclick=blueclick
+purple.onclick=purpleclick
+green.onclick=greenclick
+red.onclick=redclick
+
+// RENG 100 DENE OLSAYDI NECE YAZACAĞIM HAQDA HEÇ BİR FİKRİM YOXDU :(
+
