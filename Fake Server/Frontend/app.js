d9c968172e63b2c7f73cac6bcfde673c41460855
ayxan65 <memmedliayxan0@gmail.com>
2024-03-30 13:34:35 +0400

    server added

--- Fake Server/Frontend/app.js
+++ Fake Server/Frontend/app.js
@@ -1,25 +1,58 @@
 const url = "http://localhost:3000/users";
 const Users = document.querySelector(".users");
 
-const WriteData = async ()=>{
-    const {data} = await axios(url);
-    data.forEach(({name,surname,description,img} )=> {
+/**** await axios ile ******/
 
-        Users.innerHTML +=`<div class="user">
-        <h3>
-        ${name}
-        </h3>
 
-        <h4>
-        ${surname}
-        </h4>
-        <p>
-            ${description}
-        </p>
-        <img src="${img}" alt="">
+// const WriteData= async()=>{
+//     const {data}=await axios(url);
 
-    </div>`
-    });
-}
-WriteData();
+//     data.forEach(({name,surname,description,img})=> {
+        
+//         Users.innerHTML +=`<div class="user">
+//         <h3>
+//         ${name}
+//         </h3>
+
+//         <h4>
+//         ${surname}
+//         </h4>
+//         <p>
+//             ${description}
+//         </p>
+        
+//         <img src="${img}" alt="">
+
+//     </div>`
+        
+//     });
+// }
+// WriteData();
+
+
+
+
+
+/******* Fetch ile ********/
+
+
+// const writeDataWithFetch = () => {
+//     const data = fetch(url);
+    
+//     data.then(response => response.json())
+//         .then(data => {       
+//             data.forEach(({ name, surname, description, img }) => {
+//                 Users.innerHTML += `
+//                     <div class="user">
+//                         <h3>${name}</h3>
+//                         <h4>${surname}</h4>
+//                         <p>${description}</p>
+//                         <img src="${img}" alt="">
+//                     </div>`;
+//             });
+//         });
+//  }
+ 
+//  writeDataWithFetch();
+ 
 
