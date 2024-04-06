41d4ef285e222cb4bf156eead9df7c188a4db716
ayxan65 <memmedliayxan0@gmail.com>
2024-04-06 20:32:26 +0400

    part 1 added

--- AYtap.az/Frontend/js/index.js
+++ AYtap.az/Frontend/js/index.js
@@ -1,26 +1,34 @@
-const url ="http://localhost:3000/";
+const url ="http://localhost:3000/cars/";
 const button1 = document.querySelector("#btn1");
 const button2 = document.querySelector("#btn2");
 const cards=document.getElementById("cards")
 
 const GetData =async()=>{
     const {data} =await axios(url);
-    console.log(data);
-data.forEach(({brand,model,engine,price,kilometers})=>{  cards.innerHTML+=`<div class="card">
+    data.forEach(({brand,image,model,engine,price,kilometers,currency})=>{  cards.innerHTML+=`<div class="card">
 <a href="#"></a>
-<img src="./images/mercedes.webp" alt="">
+<img src="${image}" alt="">
 <div class="specs">
-<h2>${price}</h3>
+<h2>${price}${currency}</h3>
 <p>${brand}</p>
 <p>${model}</p>
 <p>${engine}</p>
 <p>${kilometers}</p>
-</div>
+</div> 
 </div>
 </div>`})
 
    
 }
+
+button1.onclick=()=>{
+    let ad = prompt("istifadeçi adınınzı daxil edin:");
+    let password = prompt("parolunuzu daxil edin:");
+}
+cards.style.display="flex";
+cards.style.flexWrap="wrap"
+cards.style.width="1200px"
+cards.style.margin="30px auto"
 GetData();
 
 
