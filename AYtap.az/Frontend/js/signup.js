68fcb9f3292bdc45503af4c04dcec495b1876fae
ayxan65 <memmedliayxan0@gmail.com>
2024-04-11 12:15:56 +0400

    part2

--- AYtap.az/Frontend/js/signup.js
+++ AYtap.az/Frontend/js/signup.js
@@ -1,33 +1,3 @@
-// const url = "http://localhost:3000/cars/";
-// const url2 = "http://localhost:3000/Users/";
-// const SubmitButton = document.querySelector("#SubmitBtn");
-// const NameInput = document.querySelector("#name");
-// const UserInput = document.querySelector("#username");
-// const PassInput = document.querySelector("#pass");
-
-// SubmitButton.addEventListener("click", (e) => {
-//     e.preventDefault()
-//     const name = NameInput.value;
-//     const username = UserInput.value;
-//     const password = PassInput.value;
-
-//     const newUser = {
-//         name,
-//         username,
-//         password
-//     };
-
-//      axios.post(url2,newUser).then((res)=>{
-//         console.log(res);
-//         alert("istifadeçi elave olundu")
-//      })
-// });
-
-
-
-
-
-
 const url2 = "http://localhost:3000/Users/";
 const SubmitButton = document.querySelector("#SubmitBtn");
 const NameInput = document.querySelector("#name");
@@ -45,20 +15,34 @@ SubmitButton.addEventListener("click", (e) => {
         username,
         password
     };
-
-    // Store user data in localStorage
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-    users.push(newUser);
-    localStorage.setItem('users', JSON.stringify(users));
-
-    // Post user data to the server
-    axios.post(url2, newUser)
-        .then((res) => {
-            console.log(res);
-            alert("İstifadəçi əlavə olundu");
+ const getdata=async()=>{
+    const {data}=await axios(url2);
+    data.forEach(({username})=>{
+        if(UserInput.value == username){
+            
+                
+                alert("bele bir istifadeçi var");
+            
+            
+            
+        }
+    })
+ }
+ if(name && username && password){
+     
+     axios.post(url2, newUser)
+     .then((res) => {
+         console.log(res);
+         alert("İstifadəçi əlavə olundu");
         })
         .catch((error) => {
             console.error('Error:', error);
             alert("Xəta baş verdi. İstifadəçi əlavə olunmadı.");
         });
+    }
+    else{
+        alert("xanaları tam doldurun")
+    }
+    getdata();
+
 });
