6c20a62db59ca43f835932be8312865bd7dd6827
ayxan65 <memmedliayxan0@gmail.com>
2024-04-12 17:40:05 +0400

    new feature added

--- AYtap.az/Frontend/js/signup.js
+++ AYtap.az/Frontend/js/signup.js
@@ -4,45 +4,45 @@ const NameInput = document.querySelector("#name");
 const UserInput = document.querySelector("#username");
 const PassInput = document.querySelector("#pass");
 
-SubmitButton.addEventListener("click", (e) => {
-    e.preventDefault()
+SubmitButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+
     const name = NameInput.value;
     const username = UserInput.value;
     const password = PassInput.value;
 
-    const newUser = {
-        name,
-        username,
-        password
-    };
- const getdata=async()=>{
-    const {data}=await axios(url2);
-    data.forEach(({username})=>{
-        if(UserInput.value == username){
-            
-                
-                alert("bele bir istifadeçi var");
-            
-            
-            
-        }
-    })
- }
- if(name && username && password){
-     
-     axios.post(url2, newUser)
-     .then((res) => {
-         console.log(res);
-         alert("İstifadəçi əlavə olundu");
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-            alert("Xəta baş verdi. İstifadəçi əlavə olunmadı.");
-        });
+    if (!name || !username || !password) {
+        alert("Xanaları tam doldurun.");
+        return;
     }
-    else{
-        alert("xanaları tam doldurun")
-    }
-    getdata();
 
+    try {
+        const { data } = await axios(url2);
+        const existingUser = data.find(user => user.username === username);
+        if (existingUser) {
+            Swal.fire({
+                title: "Belə bir istifadəçi var",
+                showClass: {
+                    popup: `
+                        animate__animated
+                        animate__fadeInUp
+                        animate__faster
+                    `
+                },
+                hideClass: {
+                    popup: `
+                        animate__animated
+                        animate__fadeOutDown
+                        animate__faster
+                    `
+                }
+            });
+        } else {
+            await axios.post(url2, { name, username, password });
+            alert("İstifadəçi əlavə olundu.");
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert("Xəta baş verdi. İstifadəçi əlavə olunmadı.");
+    }
 });
