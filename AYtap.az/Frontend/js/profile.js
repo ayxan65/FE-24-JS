8f2bd5a466120b5b112206052e6c1c32103ce0db
ayxan65 <memmedliayxan0@gmail.com>
2024-04-14 20:53:31 +0400

    part 2 complected

--- AYtap.az/Frontend/js/profile.js
+++ AYtap.az/Frontend/js/profile.js
@@ -9,38 +9,40 @@ const box=document.querySelector(".box");
 const h1 =document.getElementById("h1");
 const h2 =document.getElementById("h2");
 const cardata = document.querySelector(".cardata");
-const H = document.getElementById("hhh");
+
 
 const writedata = async () => {
   try {
     const response = await axios.get(url);
     const data = response.data;
     
-    button.addEventListener("click", (e) => {
+    button.addEventListener("click", (e) => { 
       e.preventDefault();
       const ad = adinput.value;
       const pass = passinput.value;
       
-      
-      const user = data.find((user) => user.username === ad && user.password === pass);
-      
-      if (user) {
-        h1.style.display="block";
-        h2.style.display="none";
-        div.style.display = "block";
-        box.style.display="none";
-        cardata.style.display="block";
-        H.style.display="block";
-        div.innerHTML = `<div class="info">
-        <h2>id: ${user.id}</h2>
-            <h3> username: ${user.username}</h3>
-            <h3>name: ${user.name}</h3>
-            <a href="./addcar.html"> <button id="addcar">Add Car</button></a>
-            <a href="./index.html"> <button id="skip">Geri</button></a>
-            </div>`;
-            
-      } else {
-        alert("istifadeçi yoxdur."); 
+      try {
+        const user = data.find((user) => user.username === ad && user.password === pass);
+        
+        if (user) {
+          h1.style.display="block";
+          h2.style.display="none";
+          div.style.display = "block";
+          box.style.display="none";
+          cardata.style.display="block";
+          div.innerHTML = `<div class="info">
+          <h2>id: ${user.id}</h2>
+              <h3> username: ${user.username}</h3>
+              <h3>name: ${user.name}</h3>
+              <a href="./addcar.html"> <button id="addcar">Add Car</button></a>
+              <a href="./index.html"> <button id="skip">Geri</button></a>
+              </div>`;
+              
+        } else {
+          alert("istifadeçi yoxdur."); 
+        }
+      } catch (error) {
+        console.error("going wrong",error);
       }
     });
   } catch (error) {
@@ -51,15 +53,55 @@ const writedata = async () => {
 writedata();
 
 
+// const writedata = async () => {
+//   try {
+//     const response = await axios.get(url);
+//     const data = response.data;
+    
+//     button.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       const ad = adinput.value;
+//       const pass = passinput.value;
+      
+      
+//       const user = data.find((user) => user.username === ad && user.password === pass);
+      
+//       if (user) {
+//         h1.style.display="block";
+//         h2.style.display="none";
+//         div.style.display = "block";
+//         box.style.display="none";
+//         cardata.style.display="block";
+//         H.style.display="block";
+//         div.innerHTML = `<div class="info">
+//         <h2>id: ${user.id}</h2>
+//             <h3> username: ${user.username}</h3>
+//             <h3>name: ${user.name}</h3>
+//             <a href="./addcar.html"> <button id="addcar">Add Car</button></a>
+//             <a href="./index.html"> <button id="skip">Geri</button></a>
+//             </div>`;
+            
+//       } else {
+//         alert("istifadeçi yoxdur."); 
+//       }
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// writedata();
+
+
 const getdata =async()=>{
  const {data}= await axios(url2);
  
- data.forEach(({brand,model,engine,price,currency})=>{
+ data.forEach(({brand,model,engine})=>{
   cardata.innerHTML += `   <div class="car">
   <h2>${brand}</h2>
   <h3>${model}</h3>
   <h3>${engine}</h3>
-  <h3>${price} ${currency}</h3>
+
 
   
   <hr>
