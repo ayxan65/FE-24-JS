7551818aeda61971902934810659e3630fff8a3d
ayxan65 <memmedliayxan0@gmail.com>
2024-05-03 21:59:28 +0400

    added

--- React_Project/src/App.jsx
+++ React_Project/src/App.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
 import NavBar from './NavBar.jsx';
-import Banner from "./Banner.jsx";
 import { Route , Routes } from 'react-router';
-import image from './assets/image.svg';
-import pcimg from './assets/pcimg.svg';
-import { Link } from 'react-router-dom';
 import './css/homestyle.css';
 import İndex from './Home/index.jsx';
 import Feature from './Features/Feature.jsx';
 
-
 function App() {
 
-
 return(
-    <div>
+    <>
         <NavBar/>
         <Routes>
             <Route path='/' element={<İndex/>}></Route>
 
             <Route path='/Features' element={<Feature/>}></Route>
+            
         </Routes>
-        
-        
-        
-    </div>
+      
+      </>
 )
 }
 
--- React_Project/src/Work.jsx
+++ React_Project/src/Work.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Work = () => {
   return (
     <div>
-        <div className="main">
-            <div className="container">
-                
-
+        <div className="main1">
+            <div className="container1">
+                <div className="left1">
+                <div className="title1">How we work</div>
+                <div className="footerr">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                </div>
+                <div className="getbtn">
+                <Link to={'/'} className='Getbtn'>Get in touch with us</Link>
+                </div>
+                </div>
+              
             </div>
         </div>
     </div>
