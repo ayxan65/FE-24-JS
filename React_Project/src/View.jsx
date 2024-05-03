d36202f9c49a5f7d69c5284b306fe7da8b0c0205
ayxan65 <memmedliayxan0@gmail.com>
2024-05-03 23:22:47 +0400

    added

--- React_Project/src/View.jsx
+++ React_Project/src/View.jsx
@@ -16,7 +16,8 @@ const View = () => {
       <div className="leftright">
       <div className="left2">
            <div className="opacityimg"> 
-           
+           <p className='opp'>Workhub office Webflow Webflow Design</p>
+           <p className='lorem'>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam </p>
            </div>
         </div>
       </div>
