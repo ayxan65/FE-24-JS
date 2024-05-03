e33927889caa0a035513aa3fbc4482f0c1c7a5f6
ayxan65 <memmedliayxan0@gmail.com>
2024-05-03 22:45:54 +0400

    ade

--- React_Project/src/View.jsx
+++ React_Project/src/View.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaArrowRight } from 'react-icons/fa';
+import Rectangle from './assets/Rectangle 1316.svg'
 
 const View = () => {
   return (
@@ -12,6 +13,11 @@ const View = () => {
         <FaArrowRight className='arrlink'></FaArrowRight>
         </div>
     </div>
+      <div className="leftright">
+      <div className="left2">
+            <img src={Rectangle} alt="" />
+        </div>
+      </div>
     </div>
     </>
   )
