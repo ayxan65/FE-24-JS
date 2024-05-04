c4c371861aa74e9f3afb94d7484ec0da85d870dc
ayxan65 <memmedliayxan0@gmail.com>
2024-05-04 15:41:33 +0400

    added

--- React_Project/src/Desing.jsx
+++ React_Project/src/Desing.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import icon1 from './assets/Icon.svg';
-
+import icon2 from './assets/ruler.svg';
+import icon3 from './assets/ok.svg';
+import icon4 from './assets/Icon8.svg';
+import icon5 from './assets/some.svg';
+import icon6 from './assets/ok2.svg'; 
 const Desing = () => {
   return (
     <>
@@ -14,11 +18,17 @@ const Desing = () => {
                 <p>Uses Client First</p> 
                 <div>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. </div>
                 </div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
-                <div className="box"></div>
+                <div className="box"><img src={icon3} alt="" />
+                <p>Hands-on approach</p> 
+                <div>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. </div></div>
+                <div className="box"><img src={icon4} alt="" /> <p>Quick Delivery</p> 
+                <div>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. </div></div>
+                <div className="box"><img src={icon5} alt="" /> <p>Template Customization</p> 
+                <div>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. </div></div>
+                <div className="box"><img src={icon6} alt="" /> <p>24/7 Support</p> 
+                <div>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. </div></div>
+                <div className="box"><img src={icon2} alt="" /> <p>Two Free Revision Round</p> 
+                <div>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. </div></div>
             </div>
         </div>
         </div>
