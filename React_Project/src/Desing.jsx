f1d4a47ff2b2e4ec995849844f998dd9862f00af
ayxan65 <memmedliayxan0@gmail.com>
2024-05-04 15:05:50 +0400

    commit

--- React_Project/src/Desing.jsx
+++ React_Project/src/Desing.jsx
@@ -11,7 +11,9 @@ const Desing = () => {
             <div className='solve'><p>Design that solves</p> <p>problems, one product at</p> <p>a time</p></div>
             <div className="boxes">
                 <div id='salam' className="box"><img src={icon1} alt="" />
-                <p>Uses Client First</p> </div>
+                <p>Uses Client First</p> 
+                <div>Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam sed faucib turpis eu gravida mi. Pellentesque et velit aliquam sed mi. </div>
+                </div>
                 <div className="box"></div>
                 <div className="box"></div>
                 <div className="box"></div>
