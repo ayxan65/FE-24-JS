c1be69726988ea777bbc52637e9afcf1359f4fbb
ayxan65 <memmedliayxan0@gmail.com>
2024-05-04 20:22:45 +0400

    commit

--- React_Project/src/View.jsx
+++ React_Project/src/View.jsx
@@ -5,24 +5,12 @@ import ourimg from './assets/ourwork.svg';
 const View = () => {
   return (
     <>
-    <div className="bigdiv">
-    <div className="ourview">
-        <div className="viewourproject">View our projects</div>
-        <div className="btncls">
-        <div className="Link">View More</div>
-        <img src={ourimg} alt="" />
-        <FaArrowRight className='arrlink'></FaArrowRight>
-        </div>
-    </div>
-      <div className="leftright">
-      <div className="left2">
-           <div className="opacityimg"> 
-           <p className='opp'>Workhub office Webflow Webflow Design</p>
-          
-           </div>
-        </div>
-      </div>
-    </div>
+
+       <div className="question-container">
+       <img src={ourimg} alt="" />
+       </div>
+   
+   
     </>
   )
 }
