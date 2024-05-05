8346923714f9ec8b52a6078515817d717e02d65f
ayxan65 <memmedliayxan0@gmail.com>
2024-05-05 11:25:45 +0400

    added

--- React_Project/src/Footer.jsx
+++ React_Project/src/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Logo from './assets/Logo.svg';
+import face from './assets/face.svg';
+import insta from './assets/insta.svg';
+import tw from './assets/tw.svg';
+import linkedn from './assets/linkedn.svg';
 
 const Footer = () => {
   return (
@@ -11,9 +15,6 @@ const Footer = () => {
             <div className="con-left-footer">
             We are always open to discuss your project and improve your online presence.
             </div>
-            </div>
-           
-        </div>
             <div className="yellow-div">
 <div className="infosfoot">
     <div className="emails">
@@ -24,7 +25,23 @@ const Footer = () => {
     <div className="pinfo">0927 6277 28525</div></div>
 </div>
             </div>
+            </div>
           
+           <div className="right-con">
+          <div className="title-right-con">
+          <div className="hrc">Lets Talk!</div>
+          </div>
+          <div className="foot-right-con">
+          We are always open to discuss your project, improve your online presence and help with your UX/UI design challenges.
+          </div>
+          <div className="logos-right-con">
+            <img src={face} alt="" />
+            <img src={insta} alt="" />
+            <img src={tw} alt="" />
+            <img src={linkedn} alt="" />
+          </div>
+           </div>
+        </div>
     </div>
     
     </>
